c6fed19db9a1b920755b740228c1a9bbdaf44605
Semko1010 <semir01020@gmail.com>
2021-11-05 12:26:17 +0100

    v1.0

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let name = document.getElementById("name")
+let email = document.getElementById("email")
+let message = document.getElementById("message")
+
+function send() {
+    console.log(name.value)
+    console.log(email.value)
+    console.log(message.value)
+}

d841d7d5bc5c64e7109db33c309d3941fa6307eb
Semko1010 <semir01020@gmail.com>
2021-11-06 22:59:19 +0100

    v1.9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
 let ul = document.getElementById("ul");
+let change = document.getElementById("change")
+let change1 = document.getElementById("change1")
 
 function menue() {
-
     ul.style.height = "100%"
     ul.style.opacity = "1"
+    ul.style.width = "100%"
+    change.style.opacity = "1"
+    change1.style.fontSize = "22px"
 }
 
 function back() {
     ul.style.height = "0%"
     ul.style.opacity = "0"
+    ul.style.width = "0"
+    change.style.opacity = "0.1"
+    change1.style.fontSize = "5px"
+
+
 }

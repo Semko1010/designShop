de1cd189738c8271f585670f720dedba5c0a778c
Semko1010 <semir01020@gmail.com>
2021-11-06 21:23:49 +0100

    v1.8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 let ul = document.getElementById("ul");
 
 function menue() {
-    ul.classList.toggle("ulNew");
+
+    ul.style.height = "100%"
+    ul.style.opacity = "1"
+}
+
+function back() {
+    ul.style.height = "0%"
+    ul.style.opacity = "0"
 }
